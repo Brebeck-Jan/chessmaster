80c6c4cb4d036a217600da8ccbc77f5dd0de3942
Phantomias3782 <andreas@mmacb.de>
2020-06-26 11:40:26 +0200

    transfered basic neural network - not functional

--- Andreas/Development/game.py
+++ Andreas/Development/game.py
@@ -1,15 +1,31 @@
 from chessboard import Board
 from tree import Node
 import re
+import gc
 
 class Chess(object):
 
-    def __init__(self, playercolour = "Black"):
+    def __init__(self, playercolour = "Black", agent):
 
         self.board = Board()
         self.playercolour = playercolour
+
+        # Get Chess agent
+        self.chess_agent = agent
+
+        # set up parameters
+        self.gamma = 0.9 # E [0,1]
+        self.memsize = 2000
+        self.batch_size = 256
+
+        # set up arrays for neural network learning
+        self.mem_state = np.zeros(shape = (1, 8, 8, 8))
+        self.mem_sucstate = np.zeros(shape = (1, 8, 8, 8))
+        self.mem_reward = np.zeros(shape = (1))
+        self.mem_error = np.zeros(shape = (1))
+        self.mem_episode_active = np.ones(shape = (1))
     
-    def play(self):
+    def play_against_mcts(self):
         """
         Start a chessgame.
         """
@@ -32,7 +48,7 @@ class Chess(object):
             if turn == "AI":
                 
                 # get AI move
-                move = self.agent_step()
+                move = self.mcts_step()
 
                 # pass move to Player
                 turn = "Player"
@@ -52,6 +68,68 @@ class Chess(object):
             print("Actual Board in Turn: ", number)
             print(self.board.board)
     
+    def train(self):
+        """ 
+        Method to train the Chess agent.
+        """
+
+        # keep track of turncounts
+        turncount = 0
+
+        end = False
+
+        while not end:
+
+            # get state and predicted state value
+            state = np.expand_dims(self.board.layer_board.copy(), axis = 0)
+            state_value = self.chess_agent.predict(state)
+            
+            # agent plays as White player
+            if self.board.board.turn:
+                move = self.mcts_step()
+            
+            # use myopic agent as Black player
+            else:
+                move = myopic_agent_step()
+
+            # make move
+            end, reward = self.board.move(move)
+
+            # get sucstate and predict sucstate value
+            sucstate = np.expand_dims(self.board.layer_board.copy(), axis = 0)
+            suc_state_value = self.chess_agent.predict(sucstate)
+
+            # calculate error
+            error = reward + self.gamme * suc_state_value - state_value
+            error = np.float(np.squeeze(error))
+
+            # add 1 to turncount
+            turncount += 1
+
+            # save if episode is active
+            episode_active = 0 if end else 1
+
+            # construct training sample
+            self.mem_state = np.append(self.mem_state, state, axis = 0)
+            self.mem_reward = np.append(self.mem_reward, reward)
+            self.mem_sucstate = np.append(self.mem_sucstate, sucstate, axis = 0)
+            self.mem_error = np.append(self.mem_error, error)
+            self.mem_episode_active = np.append(self.mem_episode_active, episode_active)
+
+            # clear memory if neccessary?
+            if self.mem_state.shape[0] > self.memsize:
+                
+                self.mem_state = self.mem_state[1:]
+                self.mem_reward = self.mem_reward[1:]
+                self.mem_sucstate = self.mem_sucstate[1:]
+                self.mem_error = self.mem_error[1:]
+                self.mem_episode_active = self.mem_episode_active[1:]
+            
+            # update agent every 10 steps
+            if turncount % 10 == 0:
+
+                self.update_agent()
+
     def player_step(self):
         """
         Let player make move.
@@ -77,9 +155,7 @@ class Chess(object):
         # return move
         return move
 
-        
-    
-    def agent_step(self, depth = 120):
+    def mcts_step(self, depth = 120):
         """
         Let agent (now MCTS) make a step => update to Neural Network!
         """
@@ -99,6 +175,36 @@ class Chess(object):
 
         # return move
         return move
+    
+    def myopic_agent_step(self):
+        """
+        Computer Agent who plays myopic.
+        """
+
+        move_dict = {}
+
+        for move in self.board.get_legal_move(all = True):
+
+            # get old material value
+            material_old = self.board.get_material_value()
+            
+            # run move
+            self.board.board.push(move)
+
+            # get new material value
+            material_new = self.board.get_material_value()
+
+            # undo move
+            self.board.board.pop()
+
+            # save move with value
+            move_dict[move] = material_new - material_old
+        
+        # get max move
+        max_move = max(move_dict, key = move_dict.get)
+
+        # return move
+        return max_move
     
     def reset(self):
         """
@@ -129,3 +235,56 @@ class Chess(object):
 
         # return move
         return move
+    
+    def update_agent(self):
+        """
+        Update Chess agent.
+        """
+
+        # get data
+        choice_indices, states, rewards, sucstates, episode_active = self.get_minibatch()
+
+        # calculate TD error
+        td_errors = self.agent.TD_update(states, rewards, sucstates, episode_active, gamma = self.gamma)
+
+        # update mem error
+        self.mem_error[choice_indices.tolist()] = td_errors
+    
+    def get_minibatch(self):
+        """
+        Get minibatch of experienced games.
+        """
+
+        # set sampling priorities
+        sampling_priorities = np.abs(self.mem_error) + 1e-9
+
+        sampling_probs = sampling_priorities / np.sum(sampling_priorities)
+        sample_indices = [x for x in range(self.mem_state.shape[0])]
+
+        # set choice indices
+        choice_indices = np.random.choice(
+            sample_indices,
+            min(self.mem_state.shape[0], self.batch_size),
+            p = np.squeeze(sampling_probs),
+            replace = False
+        )
+
+        # get data
+        states = self.mem_state[choice_indices]
+        rewards = self.mem_reward[choice_indices]
+        sucstates = self.mem_sucstate[choice_indices]
+        episode_active = self.mem_episode_active[choice_indices]
+
+        # return
+        return choice_indices, states, rewards, sucstates, episode_active
+
+        """
+        Funktionsweise:
+        neuronales netz lernt die state vlaues vorherzusagen, 
+        damit kann der mcts um einiges schneller errechnet werden!!!!! 
+        einfach nach der expansion die zugehörigen state values von dem neuronalen 
+        netz geben lassen!!
+
+        Was fehlt: der anfang von der learn. py also die iterationen und dann das updatedn des fiixed models
+        dann noch dass der mcts nicht komplett resettet wird jedesmal, sondern, dass die gewählte child node neue parent jnode wird und die werte erhalten bleiben!
+        """
---
+++ Chess_agent/Development/chessboard.py
@@ -0,0 +1,249 @@
+import chess
+import numpy as np
+
+# set up mapper for chessman for specific layer
+mapper = {}
+
+# Pawn (Bauer)
+mapper["p"] = 0
+mapper["P"] = 0
+
+# Rook (Turm)
+mapper["r"] = 1
+mapper["R"] = 1
+
+# Knight (Springer)
+mapper["n"] = 2
+mapper["N"] = 2
+
+# Bishop (Läufer)
+mapper["b"] = 3
+mapper["B"] = 3
+
+# Queen (Königin)
+mapper["q"] = 4
+mapper["Q"] = 4
+
+# King (König)
+mapper["k"] = 5
+mapper["K"] = 5
+
+class Board(object):
+
+    def __init__(self):
+        """
+        Setup Chess Environment
+        """
+
+        self.board = chess.Board()
+
+        self.init_layer_board()
+    
+    def get_legal_move(self, all = False):
+        """
+        Return random legal move.
+        """
+
+        # get all legal moves
+        legal_moves = [move for move in self.board.generate_legal_moves()]
+
+        if all:
+
+            # return all possible legal moves
+            return legal_moves
+
+        # get random move
+        ran_move = np.random.choice(legal_moves)
+
+        # return random move
+        return ran_move
+
+    def init_layer_board(self):
+        """
+        Initialize a numerical representation of the Board.
+        Each chessman class should be represented at an own layer.
+        """
+
+        self.layer_board = np.zeros(shape = (8, 8, 8))
+
+        # loop through board
+        for i in range(64):
+
+            row = i // 8
+            col = i % 8
+
+            piece = self.board.piece_at(i)
+
+            # No piece at given position
+            if piece == None:
+                continue
+
+            # check colour
+            elif piece.symbol().isupper():
+
+                # is white
+                colour = 1
+            
+            else:
+
+                # is black
+                colour = -1
+
+            # map piece
+            layer = mapper[piece.symbol()]
+
+            # fill layer board
+            self.layer_board[layer, row, col] = colour
+            self.layer_board[6, :, :] = 1 / self.board.fullmove_number
+        
+        # mark Layer 6, row 0 with colour | UNDERSTAND
+        if self.board.turn:
+
+            self.layer_board[6, 0, :] = 1
+        else:
+            
+            self.layer_board[6, 0, :] = -1
+
+        # update layer 7 | UNDERSTAND
+        self.layer_board[7, :, :] = 1 
+    
+    def update_layer_board(self):
+        """
+        Save old layer board and create new.
+        """
+
+        self._prev_layer_board = self.layer_board.copy()
+        self.init_layer_board()
+    
+    def get_prev_layer_board(self):
+        """
+        Return to old layer board.
+        """
+
+        self.layer_board = self._prev_layer_board.copy()
+        self._prev_layer_board = None
+    
+    def move(self, move, capture_reward_factor = 0.01):
+        """
+        Run a chess move.
+        Return if game has ended and the material balance delta.
+        capture_reward_factor adjusts reward for capturing figures.
+        """
+
+        # get actual material balance
+        act_balance = self.get_material_value()
+
+        # run move
+        self.board.push(move)
+
+        # update layer_board
+        self.update_layer_board()
+
+        # get new material balance
+        new_balance = self.get_material_value()
+
+        # calculate material balance delta
+        balance_reward = (new_balance - act_balance) * capture_reward_factor
+
+        # retrieve result from board
+        result = self.board.result()
+
+        # check if game is over and set corresponding reward
+        if result == "*":
+            
+            # game is not over
+            reward = 0
+            end = False
+        
+        elif result == "1-0":
+
+            # white (AI only plays white) wins
+            reward = 1
+            end = True
+        
+        elif result == "0-1":
+
+            # Black (opponent) wins
+            reward = -1
+            end = True
+        
+        elif result == "1/2-1/2":
+
+            # Draw
+            reward = 0 # OPTION: set to -0.5 to train programm to win!?
+            end = True
+
+        # add reward and balance reward
+        reward += balance_reward
+
+        # return
+        return end, reward
+    
+    def handle_user_move(self, user_input):
+        """
+        Transform user input in python-chess move.
+        """
+
+        # transform to python-chess move
+        move = chess.Move.from_uci(user_input)
+
+        # check if move is legal
+        moves = self.get_legal_move(all=True)
+        
+        if move in moves:
+
+            # move is legal
+            return move
+        
+        else:
+
+            # move is illegal
+            raise ValueError("move is illegal")
+
+    def get_material_value(self, option = "all"):
+        """
+        Get the material values.
+        Optional: only return material value for spec figure.
+        Values are based on changevalue to pawns (Wikipedia).
+        """
+
+        # get values
+        pawn_value  = 1 * np.sum(self.layer_board[0, :, :]) # Pawns have value 1
+        minor_value = 3 * np.sum(self.layer_board[2:4, :, :]) # Minors have value 3
+        rook_value  = 5 * np.sum(self.layer_board[1, :, :]) # Rooks have value 5
+        queen_value = 9 * np.sum(self.layer_board[4, :, :]) # Queen has value 9
+
+        # return values
+        if option == "all":
+
+            # return all
+            return pawn_value + minor_value + rook_value + queen_value
+        
+        elif option == "pawn":
+
+            return pawn_value
+        
+        elif option == "minor":
+
+            return minor_value
+        
+        elif option == "rook":
+
+            return rook_value
+        
+        elif option == "queen":
+
+            return queen_value
+        
+        else:
+
+            # No valid option selected
+            return 0
+
+    def reset_board(self):
+        """
+        Reset the chess board
+        """
+
+        self.board = chess.Board()
+        self.init_layer_board()
